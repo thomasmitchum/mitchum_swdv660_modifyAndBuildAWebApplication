d719214773ec529bb7dc1ed9b176de68a8d6f789
thomasmitchum <thomas.d.mitchum@gmail.com>
2022-01-29 13:56:12 -0500

    Update helloworld.js
    Updated statement

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Welcome to my web server\nCreated by Thomas Mitchum\n')
+   response.end('This is my webserver created by an Ansible playbook\n It has NodeJS, NPM and MongoDB \nCreated by Thomas Mitchum\n')
 }).listen(3333)
 
 // Console will print the message
